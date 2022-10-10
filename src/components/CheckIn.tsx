5fe9c10c5e96b474cb30aba88320259636e3c022
Ashlyn DeVries <ashjdevries@gmail.com>
2022-10-10 10:57:58 -0400

    Updated, but errors still exist with promises.

--- src/components/CheckIn.tsx
+++ src/components/CheckIn.tsx
@@ -2,20 +2,22 @@ import { TableCell, TableRow, Typography } from "@mui/material"
 import axios from "axios"
 import { PropsWithChildren, useEffect, useState } from "react"
 import { CheckIn, Event } from "../models/checkin"
-import { get_all_checkins_by_id } from "../services/CheckIn"
 
 export const CheckInRow = ({ checkin }: PropsWithChildren<{ checkin: CheckIn }>) => {   
-  
+  const [the_event, set_the_event] = useState<Event>();
+
   const get_all_events = async () => {
       let all_events: Array<Event> = (await axios.get("http://localhost:3001/v1/event")).data;
-      const match_events_to_checkin = all_events.filter((event) => event.id == checkin.event)
-      return match_events_to_checkin[0].alias;
+      const match_events_to_checkin = all_events.find((event) => event.id == checkin.event)
+      set_the_event(match_events_to_checkin);
   }
+
+  // useEffect(() => get_all_events(), []);
 
   return (
       <TableRow>
         <TableCell>
-          <Typography>{get_all_events().toString()}</Typography>
+          <Typography>{/*the_event.alias*/}</Typography>
         </TableCell>
         <TableCell>
           <Typography>{(new Date(+checkin.created)).toDateString()}</Typography>
