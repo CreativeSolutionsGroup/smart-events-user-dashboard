5cc26a90cadf213653001b865f5bf0b7b77a6b42
Konolo <wjacobs@cedarville.edu>
2022-10-11 16:45:40 -0400

    Added a customizable ProgressBar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css'
 import { Typography } from '@mui/material'
 import Login from './components/Login'
 import CheckInTable from './components/CheckInTable'
+import {ProgressBar} from './components/ProgressBar'
 
-function App() {
+const App = () => {
   const [checkins, set_checkins] = useState([])
 
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <Typography sx={{mt: 5}} align="center">Check In</Typography>
       <CheckInTable checkins={checkins} />
+      <ProgressBar percent={25}>Hello world!</ProgressBar>
     </div>
   )
 }
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import { PropsWithChildren, useEffect, useState } from "react"
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 10,
+    borderRadius: 5,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+        backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+        borderRadius: 5,
+        backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+    },
+}));
+
+export const ProgressBar = ({ percent, children }: PropsWithChildren<{ percent: number }>) => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            {children}
+            <BorderLinearProgress variant="determinate" value={percent} />
+        </Box>
+    );
+}
