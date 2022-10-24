95b45a822675bfe5ab3023f399865374e5ee1820
Ashlyn DeVries <ashjdevries@gmail.com>
2022-10-24 13:41:32 -0400

    Changed name to is_redeemable.

--- src/components/RewardCard.tsx
+++ src/components/RewardCard.tsx
@@ -1,15 +1,25 @@
 import { Button, Card, Chip, Box, Typography } from "@mui/material"
 import { useState } from "react";
 
-const RewardCard = ({ label, status, reward }: React.PropsWithChildren<{ label: String, status: String, reward: boolean }>) => {
+const RewardCard = ({ label, status, is_redeemable }: React.PropsWithChildren<{ label: String, status: String, is_redeemable: boolean }>) => {
 
+    /*const MyComponent = () => {
+        const some_thing = () => {
+             alert("Hello world");
+        }
+    
+        return (
+            <RewardCard on_redeem={some_thing} reward={true} />
+        );
+    }*/
+    
     const [redeem, set_redeem] = useState("Redeem");
 
     const handleClick = () => {
         set_redeem("Redeemed");
     };
 
-    if (reward) {
+    if (is_redeemable) {
         return (
             <Card sx={{display: "flex", flexDirection: "row", justifyContent: "space-between", width: "25rem", alignItems: "center", p: 1, m: 1}}>
                 <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
