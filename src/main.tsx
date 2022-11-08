233b239c42eefe9ebb192661e3f837ee5782aff2
Sebastian Neiswanger <sebastianneiswanger@gmail.com>
2022-11-08 16:56:36 -0500

    Register card exists and can send user id to db

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,11 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import axios from 'axios'
 
 const clientId = '8990298927-tftcl4kcfe52ikmu3quthlo7v3qvognu.apps.googleusercontent.com';
+
+axios.defaults.headers.common = { "Authorization": `Bearer ${sessionStorage.getItem("credential")}` };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
