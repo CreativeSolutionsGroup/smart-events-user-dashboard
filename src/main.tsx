38b0e546e539c85a256dc9c3519e30b06207de7d
spbills <spencebills0@gmail.com>
2022-11-09 09:07:38 -0500

    Added baseurl default.

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import axios from 'axios'
 const clientId = '8990298927-tftcl4kcfe52ikmu3quthlo7v3qvognu.apps.googleusercontent.com';
 
 axios.defaults.headers.common = { "Authorization": `Bearer ${sessionStorage.getItem("credential")}` };
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND ?? "http://localhost:3001/api";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
