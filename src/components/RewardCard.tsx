9dd87555a97e5c13c8a6544b87d1f650c46d0d72
spbills <spencebills0@gmail.com>
2022-11-10 17:24:01 -0500

    Added reward card action area

--- src/components/RewardCard.tsx
+++ src/components/RewardCard.tsx
@@ -1,19 +1,27 @@
-import { Card, Typography } from "@mui/material"
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material"
 import { grey } from "@mui/material/colors";
 
 const RewardCard = ({ label, is_redeemable, on_redeem }: React.PropsWithChildren<{ label: String, is_redeemable: boolean, on_redeem: Function }>) => {
   if (is_redeemable) {
     return (
-      <Card sx={{ alignItems: "center", cursor: "pointer", p: 1, m: 1, background: grey[300], outlineColor: grey[400] }}>
-        <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
+      <Card sx={{ alignItems: "center", cursor: "pointer", m: 1, background: grey[300], outlineColor: grey[400] }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
     )
 
   }
   else {
     return (
-      <Card sx={{ alignItems: "center", cursor: "pointer", p: 1, m: 1, outlineColor: grey[400] }} onClick={() => on_redeem()}>
-        <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
+      <Card sx={{ alignItems: "center", cursor: "pointer", m: 1, outlineColor: grey[400] }} onClick={() => on_redeem()}>
+        <CardActionArea>
+          <CardContent>
+            <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
     )
   }
