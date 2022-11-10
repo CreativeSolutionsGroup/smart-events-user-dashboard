35dc03f00e7a9292ec5553c45c70fe155f9d4f0a
spbills <spencebills0@gmail.com>
2022-11-10 09:29:16 -0500

    Made reward graphic cgeneric

--- src/components/RewardGraphic.tsx
+++ src/components/RewardGraphic.tsx
@@ -3,15 +3,16 @@ import {
     LinearProgress,
     Typography,
 } from '@mui/material'
-import AspectRatio from '@mui/joy/AspectRatio'
-function RewardsGraphic() {
+import { AspectRatio } from '@mui/joy'
+import { PropsWithChildren } from 'react'
+const RewardsGraphic = ({reward_status, color}: PropsWithChildren<{reward_status: string, color: string}>) => {
     return (
         <div className='RewardsGraphic'>
             <Box className='h-auto flex flex-col flex-nowrap justify-center items-center rounded-3xl'>
                 <AspectRatio ratio='1/1' className='w-11/12 mb-8'>
-                    <Box className='bg-amber-100 border-2 border-amber-500 rounded-full flex flex-col justify-center'>
-                        <Typography className='text-9xl font-bold font-poppins text-amber-500'>Gold</Typography>
-                        <Typography className='text-2xl font-bold font-poppins text-zinc-700'>Reward Status</Typography>
+                    <Box sx={{ backgroundColor: color }} className='border-2 rounded-full flex flex-col justify-center'>
+                        <Typography className='text-9xl font-bold font-poppins'>{reward_status}</Typography>
+                        <Typography className='text-2xl font-bold font-poppins'>Reward Status</Typography>
                     </Box>
                 </AspectRatio>
             </Box>
