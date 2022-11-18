a1dbc5827fc89ec103a4d92357d4cf91f5d24d6b
Jackson Isenhower <jacksonisenhower@cedarville.edu>
2022-11-18 01:06:52 -0500

    Removed unnecessary imports.

--- src/components/UserRewardDisplay.tsx
+++ src/components/UserRewardDisplay.tsx
@@ -1,10 +1,5 @@
 import { PropsWithChildren } from "react";
 import { IUserReward } from "../models/User";
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 function UserRewardDisplay({ userRewards }: PropsWithChildren< {userRewards: Array<IUserReward>} >) {
   return (
