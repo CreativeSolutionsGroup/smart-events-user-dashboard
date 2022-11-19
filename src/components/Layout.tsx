dc8cacb7aaa58e010438d7641f861e343c5de981
kcouv <kcouvion@cedarville.edu>
2022-11-19 15:33:21 -0500

    top bar overlaps google sso

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 import { check_login } from "../services/User";
 import { BottomBar } from "./BottomBar";
+import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
 
 export const Layout = ({ children }: PropsWithChildren) => {
   const navigate = useNavigate();
@@ -24,6 +26,9 @@ export const Layout = ({ children }: PropsWithChildren) => {
 
   return (
     <div>
+      <AppBar sx={{width: 1, display: "flex"}}>
+        <Button sx={{width: 1/6, color: "white", ml: "auto", mr: 1, my: 1, border: 1}} variant="outlined">Logout</Button>
+      </AppBar>
       { children }
       <BottomBar />
     </div>
