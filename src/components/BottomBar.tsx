150bebb5993a16ccc41b2855f37786047e800248
Ashlyn DeVries <ashjdevries@gmail.com>
2022-11-21 16:13:55 -0500

    Added home & stats redirect.

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -2,14 +2,17 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction"
 import HomeIcon from "@mui/icons-material/Home"
 import BarChartIcon from "@mui/icons-material/BarChart"
-import { Paper } from "@mui/material";
+import { Paper, Link } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 export const BottomBar = () => {
+  const location = useLocation();
+
   return (
     <div>
-      <BottomNavigation component={Paper} sx={{ position: "fixed", bottom: 0, minWidth: "100%" }} showLabels>
-          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction label="Stats" icon={<BarChartIcon />} />
+      <BottomNavigation value={location.pathname} component={Paper} sx={{ position: "fixed", bottom: 0, minWidth: "100%" }} showLabels>
+          <BottomNavigationAction label="Dashboard" value="/dashboard" icon={<HomeIcon />} href="/dashboard" LinkComponent={Link} />
+          <BottomNavigationAction label="Stats" value="/stats" icon={<BarChartIcon />} href="/stats" LinkComponent={Link}/>
       </BottomNavigation>
     </div>
   )
