328d9407ee4dd92694e1a4306a6c9e66a796f6b6
spbills <spencebills0@gmail.com>
2022-11-23 14:29:20 -0500

    Small refactor of reward card

--- src/components/RewardCard.tsx
+++ src/components/RewardCard.tsx
@@ -1,15 +1,20 @@
 import { Card, CardActionArea, CardContent, Typography } from "@mui/material"
 import { grey } from "@mui/material/colors";
+import { PropsWithChildren } from "react";
 
-const RewardCard = ({ label, is_redeemable, on_redeem }: React.PropsWithChildren<{ label: String, is_redeemable: boolean, on_redeem: Function }>) => {
+const ActionArea = ({label}: PropsWithChildren<{label: string}>) => (
+  <CardActionArea>
+    <CardContent>
+      <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
+    </CardContent>
+  </CardActionArea>
+)
+
+const RewardCard = ({ label, is_redeemable, on_redeem }: React.PropsWithChildren<{ label: string, is_redeemable: boolean, on_redeem: Function }>) => {
   if (is_redeemable) {
     return (
       <Card sx={{ alignItems: "center", cursor: "pointer", m: 1, background: grey[300], outlineColor: grey[400] }}>
-        <CardActionArea>
-          <CardContent>
-            <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
-          </CardContent>
-        </CardActionArea>
+        <ActionArea label={label} />
       </Card>
     )
 
@@ -17,11 +22,7 @@ const RewardCard = ({ label, is_redeemable, on_redeem }: React.PropsWithChildren
   else {
     return (
       <Card sx={{ alignItems: "center", cursor: "pointer", m: 1, outlineColor: grey[400] }} onClick={() => on_redeem()}>
-        <CardActionArea>
-          <CardContent>
-            <Typography variant="subtitle1" fontWeight="bold">{label}</Typography>
-          </CardContent>
-        </CardActionArea>
+        <ActionArea label={label} />
       </Card>
     )
   }
