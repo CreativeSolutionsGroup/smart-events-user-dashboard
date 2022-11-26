f3b0eec06473362c7dbd28efd048f698f72aedaf
spbills <spencebills0@gmail.com>
2022-11-25 19:37:51 -0500

    Fixed ios scrolling issue

--- src/components/RedeemPopup.tsx
+++ src/components/RedeemPopup.tsx
@@ -8,6 +8,7 @@ interface PopupProps { reward: IReward, show?: boolean, onClose: Function };
 export const RedeemPopup = ({ reward, show, onClose }: PropsWithChildren<PopupProps>) => {
   useEffect(() => {
     document.body.style.overflowY = show ? "hidden" : "scroll";
+    document.body.style.height = show ? "100vh" : "";
   }, [show])
 
   return (
