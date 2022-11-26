21acaf950edd74d7f8cea62d3266882838894303
spbills <spencebills0@gmail.com>
2022-11-25 20:00:30 -0500

    Overflow fix

--- src/components/RedeemPopup.tsx
+++ src/components/RedeemPopup.tsx
@@ -7,8 +7,8 @@ interface PopupProps { reward: IReward, show?: boolean, onClose: Function };
 
 export const RedeemPopup = ({ reward, show, onClose }: PropsWithChildren<PopupProps>) => {
   useEffect(() => {
-    document.body.style.overflowY = show ? "hidden" : "scroll";
     document.body.style.position = show ? "fixed" : "";
+    document.body.style.overflow = show ? "hidden" : "scroll";
   }, [show])
 
   return (
