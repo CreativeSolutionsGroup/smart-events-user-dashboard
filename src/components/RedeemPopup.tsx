81e8a86dbcc777a33bff61ee36cdea6081eb7262
spbills <spencebills0@gmail.com>
2022-11-26 09:01:51 -0500

    Fixed scroll top redeem popup

--- src/components/RedeemPopup.tsx
+++ src/components/RedeemPopup.tsx
@@ -1,16 +1,24 @@
 import { BottomPaper } from "@creativesolutionsgroup/bottom-paper"
 import { Button, Paper, Typography } from "@mui/material"
-import { PropsWithChildren, useEffect } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 import { IReward } from "../models/User"
 
 interface PopupProps { reward: IReward, show?: boolean, onClose: Function };
 
 export const RedeemPopup = ({ reward, show, onClose }: PropsWithChildren<PopupProps>) => {
+  // The last Y scroll position.
+  // This helps us set the fixed position bug.
+  const [last_scroll, set_last_scroll] = useState(window.scrollY);
+
   useEffect(() => {
-    document.body.style.top = `-${document.body.scrollTop}px`;
+    let scroll = show ? window.scrollY : last_scroll;
+    set_last_scroll(scroll);
+    document.body.style.top = show ? `-${window.scrollY}px` : "";
+
     document.body.style.position = show ? "fixed" : "";
     document.body.style.overflow = show ? "hidden" : "scroll";
     document.body.style.overscrollBehaviorY = show ? "none" : "";
+    !show && window.scrollTo(0, scroll);
   }, [show])
 
   return (
