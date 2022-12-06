979bdf67f35c2ccf1a46ee3665527e8efd1d06ba
spbills <spencebills0@gmail.com>
2022-12-06 14:47:06 -0500

    Fixed register redirection logic

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom"
 import { check_login } from "../services/User";
 
-const LoginButton = () => { 
-  
+const LoginButton = () => {
+
   const [is_logged_in, set_is_logged_in] = useState(false);
   const navigate = useNavigate();
 
@@ -19,13 +19,18 @@ const LoginButton = () => {
         onSuccess={async credentialResponse => {
           sessionStorage.setItem("credential", credentialResponse.credential!);
           axios.defaults.headers.common = { "Authorization": `Bearer ${sessionStorage.getItem("credential")}` };
-          const check_login_passed = await check_login();
-
-          if (check_login_passed) {
-            navigate("/dashboard")
-          } else {
+          try {
+            const check_login_passed = await check_login();
+            if (check_login_passed) {
+              navigate("/dashboard")
+            } else {
+              navigate("/register")
+            }
+          } catch (e) {
             navigate("/register")
           }
+
+
         }}
         onError={() => {
           alert("Login Failed");
