49cb0fb88343d3a1c211129a22da597dc7d5fdee
SebastianNeiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2022-12-19 13:56:00 -0500

    added needed functions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { PropsWithChildren, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
-import { check_login } from "../services/User";
+import { check_login, getRewardTiers } from "../services/User";
 import { BottomBar } from "./BottomBar";
 import AppBar from '@mui/material/AppBar'
 import LogoutButton from './LogoutButton'
 import { TopBar } from "./TopBar";
 import { Box } from "@mui/material";
+import { IRewardTier } from "../models/User";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   const navigate = useNavigate();
   const [is_logged_in, set_is_logged_in] = useState(true);
+  const [tiers, setTiers] = useState<Array<IRewardTier>>([]);
 
   const do_login_check = async () => {
     const login_check_passed = await check_login();
@@ -20,7 +22,12 @@ export const Layout = ({ children }: PropsWithChildren) => {
     } 
   };
 
+  const get_reward_tiers = async () => {
+    setTiers(await getRewardTiers());
+  }
+
   useEffect(() => { do_login_check() }, []);
+  useEffect(() => { get_reward_tiers() }, []);
 
   return (
     <div>
